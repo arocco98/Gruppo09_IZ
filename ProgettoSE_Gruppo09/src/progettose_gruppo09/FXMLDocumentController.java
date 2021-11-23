035177961443bd0c1f480c5f8055689d5607c28f
luigi <luigi@omen.lan>
2021-11-23 20:17:50 +0100

    Pattern recognition implemented

--- ProgettoSE_Gruppo09/src/progettose_gruppo09/FXMLDocumentController.java
+++ ProgettoSE_Gruppo09/src/progettose_gruppo09/FXMLDocumentController.java
@@ -6,6 +6,8 @@ package progettose_gruppo09;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -30,7 +32,49 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private void insert(ActionEvent event) {
-        System.out.println("Hello World!");
+        if(elementTextField.textProperty().isNotEmpty().get()) {
+            
+            // pattern for real and imaginary part
+            Pattern p1 = Pattern.compile("(.*)([+-].*)j");
+            // pattern for only real part
+            Pattern p2 = Pattern.compile("(.*)[^j]");
+            // pattern for only imaginary part
+            Pattern p3 = Pattern.compile("(.*)j");
+            
+            double real = 0.0, imaginary = 0.0;
+            
+            // matcher for real and imaginary part
+            Matcher m1 = p1.matcher(elementTextField.getText().replaceAll("\\s+",""));
+            // matcher for only real part
+            Matcher m2 = p2.matcher(elementTextField.getText().replaceAll("\\s+",""));
+            // matcher for only imaginary part
+            Matcher m3 = p3.matcher(elementTextField.getText().replaceAll("\\s+",""));
+            try {
+                if (m1.matches()) { // if the user input matches the required pattern
+                real = Double.parseDouble(m1.group(1)); // extract the real part
+                imaginary = Double.parseDouble(m1.group(2)); // extract the imaginary part
+                Complex c = new Complex(real, imaginary);
+                elementTextField.clear();
+                System.out.println(c);
+                }
+                else if(m2.matches()) {
+                    real = Double.parseDouble(m2.group(1)); // extract the real part
+                    Complex c = new Complex(real, imaginary);
+                    System.out.println(c);
+                }
+                else if(m3.matches()) {
+                    imaginary = Double.parseDouble(m3.group(1)); // extract the imaginary part
+                    Complex c = new Complex(real, imaginary);
+                    System.out.println(c);
+                }
+            } catch(Exception e) {
+                elementTextField.setText("Use the notation: a + bj");
+            }
+            
+        }
+        else {
+            elementTextField.setText("Invalid input!");
+        }
     }
 
     @FXML
--- ProgettoSE_Gruppo09/src/progettose_gruppo09/ProgettoSE_Gruppo09.java
+++ ProgettoSE_Gruppo09/src/progettose_gruppo09/ProgettoSE_Gruppo09.java
@@ -15,13 +15,13 @@ import javafx.stage.Stage;
  * @author gruppo09
  */
 public class ProgettoSE_Gruppo09 extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -32,5 +32,5 @@ public class ProgettoSE_Gruppo09 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
