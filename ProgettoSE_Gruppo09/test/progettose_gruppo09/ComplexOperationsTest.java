bfcb4f785f9070645fde25dac660ed0639bab9a5
memol <memol@DESKTOP-0NM8HUM.mshome.net>
2021-11-23 16:34:21 +0100

    Test somma

--- ProgettoSE_Gruppo09/test/progettose_gruppo09/ComplexOperationsTest.java
+++ ProgettoSE_Gruppo09/test/progettose_gruppo09/ComplexOperationsTest.java
@@ -15,15 +15,51 @@ public class ComplexOperationsTest {
 
     @Test
     public void testComplexSum(){
-        Complex a = new Complex(4.0, 2.0);
-        Complex b = new Complex(3.0, 1.0);
-        Complex expected = new Complex(7.0, 3.0);
-        assertEquals(expected, ComplexOperations.complexSum(a, b));
+        
+        //This case is used to test that the sum between two numbers with
+        //real part and imaginary part equal and opposite sign gives zero 
+        assertEquals(new Complex(0.0, 0.0), ComplexOperations.complexSum(new Complex(4.0, 2.0), new Complex(-4.0, -2.0)));
+        
+        //This case is used to test that the sum between two positive
+        //numbers with imaginary part equal to zero 
+        assertEquals(new Complex(15.0, 0.0), ComplexOperations.complexSum(new Complex(8.0, 0.0), new Complex(7.0, 0.0)));
+        
+        //This case is used to test that the sum between two positive 
+        //numbers with real part equal to zero 
+        assertEquals(new Complex(0.0, 20.0), ComplexOperations.complexSum(new Complex(0.0, 17.0), new Complex(0.0, 3.0)));
+        
+        //This case is used to test that the sum between two numbers with
+        //imaginary part equal to zero and real part with opposite sign 
+        assertEquals(new Complex(-19.0, 0.0), ComplexOperations.complexSum(new Complex(4.0, 0.0), new Complex(-23.0, 0.0)));
+        
+        //This case is used to test that the sum between two numbers with
+        //real part equal to zero and imaginary part with opposite sign 
+        assertEquals(new Complex(0.0, -16.0), ComplexOperations.complexSum(new Complex(0.0, -20.0), new Complex(0.0, 4.0)));
+        
+        //This case is used to test that the sum between two positive numbers with the first
+        //having imaginary part equal to zero and the second having real part equal to zero
+        assertEquals(new Complex(14.0, 35.0), ComplexOperations.complexSum(new Complex(14.0, 0.0), new Complex(0.0, 35.0)));
+        
+        //This case is used to test that the sum between two positive numbers with the first
+        //having real part equal to zero and the second having imaginary part equal to zero 
+        assertEquals(new Complex(32.0, 46.0), ComplexOperations.complexSum(new Complex(0.0, 46.0), new Complex(32.0, 0.0)));
+        
+        //This case is used to test that the sum between two negative numbers with the first
+        //having imaginary part equal to zero and the second having real part equal to zero
+        assertEquals(new Complex(-14.0, -35.0), ComplexOperations.complexSum(new Complex(-14.0, 0.0), new Complex(0.0, -35.0)));
+        
+        //This case is used to test that the sum between two negative numbers with the first
+        //having real part equal to zero and the second having imaginary part equal to zero
+        assertEquals(new Complex(-32.0, -46.0), ComplexOperations.complexSum(new Complex(0.0, -46.0), new Complex(-32.0, 0.0)));
+        
+        //This case is used to test that the sum between two positive numbers with
+        //real part and imaginary part having decimal 
+        assertEquals(new Complex(12.1, 7.8), ComplexOperations.complexSum(new Complex(11.6, 7.0), new Complex(0.5, 0.8)));
+                
+        //This case is used to test that the sum between a negative number and
+        //a positive one with real part and imaginary part having decimal
+        assertEquals(new Complex(-11.1, -6.2), ComplexOperations.complexSum(new Complex(-11.6, -7.0), new Complex(0.5, 0.8)));
     }  
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
 }
