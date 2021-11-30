7ea52069fbabde34519d86b880252f593db19009
memol <memol@DESKTOP-0NM8HUM.lan>
2021-11-30 12:34:53 +0100

    VariablesValueEsception and SumVariableCommand first implementation

---
+++ ProgettoSE_Gruppo09/src/exceptions/VariablesValueException.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+/**
+ *
+ * @author gruppo09
+ */
+public class VariablesValueException extends Exception {
+
+    /**
+     * Creates a new instance of <code>VariableValueException</code> without
+     * detail message.
+     */
+    public VariablesValueException() {
+        super();
+    }
+
+}
